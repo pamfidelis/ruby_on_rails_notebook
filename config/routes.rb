19f3eb775a09df93d243ec9e283ded1f79de57a4
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-03-26 19:25:21 -0300

    hint routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :contacts
   resources :kinds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Para acessar todos os verbos HTTP disponível
+  # http://localhost:3000/rails/info/routes
 
   root 'home#index'
 end
