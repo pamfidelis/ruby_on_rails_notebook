86522ce458d5d453d21e49408ae167cc350c0f13
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-03-26 19:31:43 -0300

    more hint routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :addresses
-  get 'home/index'
+  get 'home/index' # para especificar |||  get '/bemvido' => 'home#index'
 
-  resources :phones
-  resources :contacts
+  resources :phones # cria 8 rotas automáticas
+  resources :contacts #, except/only: [:edit] - caso seja necessário excluir ou especificar uma rota
   resources :kinds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Para acessar todos os verbos HTTP disponível
