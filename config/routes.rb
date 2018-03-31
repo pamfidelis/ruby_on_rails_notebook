700fb9ab64105891ebeb77081ec89642317976f0
Pamela Fidelis <fidelis.pamella@gmail.com>
2018-03-31 12:46:18 -0300

    (del) Phones e Addresses

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :addresses
   get 'home/index' # para especificar |||  get '/bemvido' => 'home#index'
 
-  resources :phones # cria 8 rotas automáticas
   resources :contacts #, except/only: [:edit] - caso seja necessário excluir ou especificar uma rota
-  resources :kinds
+  resources :kinds # cria 8 rotas automáticas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Para acessar todos os verbos HTTP disponível
   # http://localhost:3000/rails/info/routes
